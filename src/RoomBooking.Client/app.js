f0bdd9c1a93a702965f4b7198bc2f0b3dd8dbf11
Raul Arrieta Brouard <raul.arrieta@gmail.com>
2016-11-02 23:30:13 +0100

    added electron client

---
+++ src/RoomBooking.Client/app.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var app = require('electron').app;
+var browser = require('electron').BrowserWindow;
+var client = require('electron-connect').client;
+
+app.on('ready', function () {
+
+    var mainWindow = new browser({
+        width: 800,
+        height: 600
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    client.create(mainWindow);
+});
---
+++ src/RoomBooking.Client/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var electron = require('electron-connect').server.create();
+    
+gulp.task('run', function () {
+
+    // Start browser process 
+    electron.start();
+
+    // Restart browser process 
+    gulp.watch('app.js', electron.restart);
+
+    // Reload renderer process 
+    gulp.watch(['index.js', 'index.html'], electron.reload);
+});
